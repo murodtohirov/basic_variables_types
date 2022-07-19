8670b59bf52618a7f42893381b90abb5c02bcc19
Пользователь <murodjontohirov2701@gmail.com>
2022-07-19 13:09:48 +0500

    var10

--- bin/var10.dart
+++ bin/var10.dart
@@ -8,5 +8,11 @@ print your last name, then print your first name at the bottom.
 */
 
 void main() {
+  String x = 'first name';
+  String y = 'last name';
+  x = ' Murod ';
+  y = ' Toxirov';
+  print(y);
+  print(x);
   // write your code here
 }
